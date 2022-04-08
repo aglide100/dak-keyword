2a7da1b9f62c9b2f68aee5972486031bbae0bf2c
aglide100 <aglide100@gmail.com>
2022-04-08 14:51:54 +0900

    container wip

--- cmd/runnerd/main.go
+++ cmd/runnerd/main.go
@@ -3,6 +3,13 @@ package main
 import (
 	"log"
 	"os"
+	"sync"
+
+	"context"
+
+	"golang.org/x/sync/errgroup"
+
+	"github.com/aglide100/go-scraper/pkg/runner/container"
 )
 
 func main() {
@@ -13,6 +20,20 @@ func main() {
 
 func realMain() error {
 	log.Printf("run runnerd")
+	var wait sync.WaitGroup
+    wait.Add(2)
 
-	return nil
+	wg, ctx := errgroup.WithContext(context.Background())
+	_ = ctx
+
+	wg.Go(func() error {
+		err := container.RunSomeContainer()
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	return wg.Wait()
 }
