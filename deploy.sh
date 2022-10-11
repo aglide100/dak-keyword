ccc2a465c5d446228e32d48acde4f2eaa522b25c
aglide100 <aglide100@gmail.com>
2022-10-11 13:42:09 +0900

    fix error

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # docker stack rm keyword
-docker service rm $(docker service ls -q)
+# docker service rm $(docker service ls -q)
 
-docker build -t apid -f ./cmd/apid/Dockerfile .
-docker build -t provisioned -f ./cmd/provisioned/Dockerfile .
-docker build -t scraped -f ./cmd/scraped/Dockerfile .
-docker build -t db -f ./cmd/db/Dockerfile .
+# docker build -t apid -f ./cmd/apid/Dockerfile .
+# docker build -t provisioned -f ./cmd/provisioned/Dockerfile .
+# docker build -t scraped -f ./cmd/scraped/Dockerfile .
+# docker build -t db -f ./cmd/db/Dockerfile .
 
 #cd ui
 #docker build -t node .
 #cd ..
 
 
-docker stack deploy --compose-file docker-compose.yml keyword
+docker stack deploy -c <(docker-compose -f docker-compose.yml config) keyword
