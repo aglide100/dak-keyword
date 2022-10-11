79d79e7abce4667fb57e9a5e16d63ca121ad2911
aglide100 <aglide100@gmail.com>
2022-10-11 13:42:09 +0900

    fix error

--- ui/src/grpc/index.tsx
+++ ui/src/grpc/index.tsx
@@ -1,2 +1,5 @@
-export const grpcWebAddr = "http://like-a-junk.com:50011";
+// import { secrets } from "docker-secret";
+
+export const grpcWebAddr = process.env.GRPCWEBADDR;
+// export const grpcWebAddr = "http://like-a-junk.com:50011";
 // export const grpcWebAddr = "https://localhost:50011";
