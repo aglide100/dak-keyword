181c88e882a088f3a8660eb9d522f4f47cba8a7d
aglide100 <aglide100@gmail.com>
2023-01-14 11:19:16 +0900

    add rewriter option at nextjs config

--- ui/next.config.js
+++ ui/next.config.js
@@ -8,46 +8,16 @@
 //     },
 // };
 
-// const nextEnv = require("next-env");
-const dotenvLoad = require("dotenv-load");
-
-dotenvLoad();
-
-// const withEnv = nextEnv();
-
-const GRPCWEBADDR = process.env.GRPCWEBADDR;
+const GRPCWEBADDR = process.env.ADDR;
 
 module.exports = {
-    reactStrictMode: true,
+    reactStrictMode: false,
     async rewrites() {
-        return {
-            fallback: [
-                {
-                    source: "/:path*",
-                    destination: "https://localhost" + `//:path*`,
-                },
-            ],
-        };
+        return [
+            {
+                source: "/pb/:path*",
+                destination: `https://` + `${GRPCWEBADDR}` + `/:path*`,
+            },
+        ];
     },
 };
-
-// };
-// const withPlugins = require("next-compose-plugins");
-// // const optimizedImages = require("next-optimized-images");
-// const withVideos = require("next-videos");
-
-// module.exports = withPlugins([
-//     [withVideos],
-//     {
-//         typescript: {
-//             // !! WARN !!
-//             // Dangerously allow production builds to successfully complete even if
-//             // your project has type errors.
-//             // !! WARN !!
-//             ignoreBuildErrors: true,
-//         },
-//         // images: {
-//         //     domains: ["img.youtube.com", "assets.example.com"],
-//         // },
-//     },
-// ]);
