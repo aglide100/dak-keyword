9654d42ec5a919190cf67cb180738ab5f14130a9
HeeJun Moon <aglide100@gmail.com>
2023-03-24 15:40:25 +0900

    modify nextjs proxy setting

--- pkg/db/slow.go
+++ pkg/db/slow.go
@@ -1,6 +1,10 @@
 package db
 
-func (db *Database) GetSimilarity(jobId string) error {
+import (
+	"fmt"
+)
+
+func (db *Database) GetSimilarity(jobId string) (error) {
 	const q =`
 	WITH article_duplicates AS (
         SELECT n1."Id" AS id1, n2."Id" AS id2, similarity(n1."Content", n2."Content") AS sim, n1."Content", n2."Content"
@@ -10,11 +14,13 @@ func (db *Database) GetSimilarity(jobId string) error {
             AND n1."Worker_id" = n2."Worker_id"
         WHERE n1."Worker_id" = $1
         ORDER BY n2."Create_at", sim DESC
-    ) SELECT id1, id2 FROM article_duplicates`
+    ) 
+    DELETE FROM article USING article_duplicates
+    WHERE article."Id" = article_duplicates.id1;`
 
-	rows, err := db.conn.Query(q, jobId)
+	rows, err := db.Conn.Query(q, jobId)
     if err != nil {
-        log.Fatal(err)
+        return err
     }
     defer rows.Close()
 
@@ -25,13 +31,13 @@ func (db *Database) GetSimilarity(jobId string) error {
         var content1, content2 string
         err = rows.Scan(&id1, &id2, &sim, &content1, &content2)
         if err != nil {
-            log.Fatal(err)
+            return err
         }
         res = append(res, fmt.Sprintf("%s %s %f %s %s", id1, id2, sim, content1, content2))
     }
     if err := rows.Err(); err != nil {
-        log.Fatal(err)
+        return err
     }
 
-    results <- res
+    return nil
 }
