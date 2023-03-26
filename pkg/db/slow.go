7180e36d59956454cba93432997d0d779f77bc9f
HeeJun Moon <aglide100@gmail.com>
2023-03-26 14:57:49 +0900

    testing

--- pkg/db/slow.go
+++ pkg/db/slow.go
@@ -1,10 +1,6 @@
 package db
 
-import (
-	"fmt"
-)
-
-func (db *Database) GetSimilarity(jobId string) (error) {
+func (db *Database) GetSimilarity(workerId string) (error) {
 	const q =`
 	WITH article_duplicates AS (
         SELECT n1."Id" AS id1, n2."Id" AS id2, similarity(n1."Content", n2."Content") AS sim, n1."Content", n2."Content"
@@ -13,29 +9,14 @@ func (db *Database) GetSimilarity(jobId string) (error) {
             AND n1."Content" % n2."Content"
             AND n1."Worker_id" = n2."Worker_id"
         WHERE n1."Worker_id" = $1
+        AND similarity(n1."Content", n2."Content") >= 0.8
         ORDER BY n2."Create_at", sim DESC
     ) 
     DELETE FROM article USING article_duplicates
     WHERE article."Id" = article_duplicates.id1;`
 
-	rows, err := db.Conn.Query(q, jobId)
+	_, err := db.Conn.Query(q, workerId)
     if err != nil {
-        return err
-    }
-    defer rows.Close()
-
-    var res []string
-    for rows.Next() {
-        var id1, id2 string
-        var sim float64
-        var content1, content2 string
-        err = rows.Scan(&id1, &id2, &sim, &content1, &content2)
-        if err != nil {
-            return err
-        }
-        res = append(res, fmt.Sprintf("%s %s %f %s %s", id1, id2, sim, content1, content2))
-    }
-    if err := rows.Err(); err != nil {
         return err
     }
 
