0c362b90b782c5edf7316333c744e0c92bbab46b
HeeJun Moon <aglide100@gmail.com>
2023-03-27 15:05:33 +0900

    seperate db func's name

--- pkg/db/article.go
+++ pkg/db/article.go
@@ -70,7 +70,7 @@ func (db *Database) WriteTweetToArticle(tweet models.TweetArticle) error {
 	return nil
 }
 
-func (db *Database) GetArticles(id string) ([]*models.Article, error) {
+func (db *Database) GetArticlesByJobID(jobID string) ([]*models.Article, error) {
 	const q = `
 	SELECT "Id", "Author", "Keyword", "Content", "Platform", "Score_happy", "Score_fear", "Score_embarrassed", "Score_sad", "Score_rage", "Score_hurt", "Score_max_value", "Score_max_name", "Create_at", "Job_id", "Worker_id"
 	FROM article
@@ -80,7 +80,89 @@ func (db *Database) GetArticles(id string) ([]*models.Article, error) {
 	ORDER BY article."Create_at" ASC
 	`
 	
-	rows, err := db.Conn.Query(q, id)
+	rows, err := db.Conn.Query(q, jobID)
+	if err != nil {
+		return nil, err
+	}
+
+	var (
+		Id string
+		Author string
+		Keyword string
+		Content string
+		Platform string
+		Score_happy string
+		Score_fear string
+		Score_embarrassed string
+		Score_sad string
+		Score_rage string
+		Score_hurt string
+		Score_max_value string
+		Score_max_name string
+		Create_at string
+		Job_id string
+		Worker_id string	
+	)
+
+	var articles []*models.Article
+
+	for rows.Next() {
+		err := rows.Scan(
+			&Id,
+			&Author,
+			&Keyword,
+			&Content,
+			&Platform,
+			&Score_happy,
+			&Score_fear,
+			&Score_embarrassed,
+			&Score_sad,
+			&Score_rage,
+			&Score_hurt,
+			&Score_max_value,
+			&Score_max_name,
+			&Create_at,
+			&Job_id,
+			&Worker_id,
+
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		article := &models.Article{
+			Id: Id,
+			Author: Author,
+			Keyword: Keyword,
+			Content: Content,
+			Platform: Platform,
+			Score_happy: Score_happy,
+			Score_fear: Score_fear,
+			Score_embarrassed: Score_embarrassed,
+			Score_sad: Score_sad,
+			Score_rage: Score_rage,
+			Score_hurt: Score_hurt,
+			Score_max_value: Score_max_value,
+			Score_max_name: Score_max_name,
+			Create_at: Create_at,
+			Job_id: Job_id,
+			Worker_id: Worker_id,
+		}
+
+		articles = append(articles, article)
+	}
+
+	return articles, nil
+}
+
+func (db *Database) GetArticlesByWorkerID(workerID string) ([]*models.Article, error) {
+	const q = `
+	SELECT "Id", "Author", "Keyword", "Content", "Platform", "Score_happy", "Score_fear", "Score_embarrassed", "Score_sad", "Score_rage", "Score_hurt", "Score_max_value", "Score_max_name", "Create_at", "Job_id", "Worker_id"
+	FROM article
+	WHERE "Worker_id" = $1
+	`
+	
+	rows, err := db.Conn.Query(q, workerID)
 	if err != nil {
 		return nil, err
 	}
