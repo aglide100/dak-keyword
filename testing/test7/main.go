7c40c531f450e3240a33a59df628d16b1e51d1b7
HeeJun Moon <aglide100@gmail.com>
2023-04-12 22:51:03 +0900

    change regex

--- testing/test7/main.go
+++ testing/test7/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	text := scraper.Preprocessing("RT @_myorin_S2: #배민트친소 #배친소 #배달의민족_트친소 #baedar_minzok 배민 이용하시는분 있나요? 저는 엄마가 사줘요.")
+	text := scraper.Preprocessing("RT 123213 @_myorin_S2: #배민트친소 #배친소 #배달의민족_트친소 #baedar_minzok 배민 이용하시는분 있나요? 저는 엄마가 사줘요.")
 
 	log.Println(text)
 	log.Println(utf8.RuneCountInString(text))
